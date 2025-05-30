79aa5d6c437b0fae4262f402a48962700935d3b7
DawrAli33285 <lemightyeagle@gmail.com>
2025-05-30 17:07:16 +0500

    new

--- src/authpage.js
+++ src/authpage.js
@@ -106,15 +106,13 @@ useEffect(() => {
    <ToastContainer containerId="authPage"/>
 
    <div className='w-full mx-auto lg:w-[98%] mt-[20px] p-[30px] bg-[#2cac4f] min-h-[95vh] lg:min-h-[95vh] flex justify-center items-center'>
-   <ins
-        ref={adRef}
-        className="adsbygoogle"
-        style={{ display: 'block' }}
-        data-ad-client="ca-pub-3780432206906063"
-        data-ad-slot="7789922698"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      ></ins>
+
+<ins class="adsbygoogle responsive_header_unit"
+     style="display:block"
+   data-ad-client="ca-pub-3780432206906063"
+         data-ad-slot="7789922698"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
     </div>
    </>
   );
